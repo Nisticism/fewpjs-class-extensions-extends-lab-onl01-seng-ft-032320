a6146b6104c19e66a3b0b33ae743c6013a4720fe
nisticism <nistacular@gmail.com>
2020-07-18 23:12:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce(function(total, element) {
+            return total + element;
+        })
+    }
+
+
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        let largestSide = this.sides.sort()[2];
+        let smallestSide = this.sides.sort()[0];
+        let middleSide = this.sides.sort()[1];
+        if (this.countSides == 3 && largestSide < smallestSide + middleSide) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+        if (this.countSides == 4 && this.sides[0] == this.sides[1] && this.sides[1] == this.sides[2] && this.sides[2] == this.sides[3]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    get area() {
+        return this.sides[0] ** 2;
+    }
+}
